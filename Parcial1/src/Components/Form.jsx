a3f53a0084945b8f3bba3560712d833210bf1ea1
LuDev01 <luisatoro0121@hotmail.com>
2024-08-15 20:52:04 -0500

    Form validations completed

--- Parcial1/src/Components/Form.jsx
+++ Parcial1/src/Components/Form.jsx
@@ -20,7 +20,7 @@ const Form = ({ setReservationData }) => {
     
 
     if (vehicleType.trim().length < 6) {
-      setErrorType('El tipo de vehiculo debe ser mayor a 6 caracteres');
+      setErrorType('El tipo de vehiculo debe ser tener por lo menos 6 caracteres');
       return;
     }else {
       setErrorType('');
@@ -39,37 +39,69 @@ const Form = ({ setReservationData }) => {
 
     alert('Reserva realizada con éxito');
   };
+  const inputStyle = {
+    width: '100%',
+    padding: '1rem',
+    marginBottom: '1rem',
+    borderRadius: '0.5rem',
+    border: '2px solid #e1e5ee',
+    fontFamily: 'inherit',
+    fontSize: '1rem',
+    color: 'white',
+    backgroundColor: 'transparent',
+    outline: 'none',
+  };
 
+  
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        <div>
         <input
           type="text"
           placeholder="Nombre del cliente"
           value={customerName}
           onChange={(e) => setCustomerName(e.target.value)}
+          style={inputStyle}
         />
           {errorName && <p>{errorName}</p>}
+          
+        </div>
+        <div>
         <input
           type="text"
           placeholder="Tipo de vehículo (ej. Sedán, SUV)"
           value={vehicleType}
           onChange={(e) => setVehicleType(e.target.value)}
+                style={inputStyle}
         />
            {errorType && <p>{errorType}</p>}
+
+        </div>
+        <div>
+
         <input
           type="date" 
           placeholder="Fecha de inicio"
           value={startDate}
           onChange={(e) => setStartDate(e.target.value)}
+                style={inputStyle}
         />
+        </div>
+        <div>
+
         <input
           type="date" 
           placeholder="Fecha de finalización"
           value={endDate}
           onChange={(e) => setEndDate(e.target.value)}
+                style={inputStyle}
         />
+        </div>
+        <div>
+
         <button type="submit">Reservar</button>
+        </div>
       </form>
 
     </div>
